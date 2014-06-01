c1c0ccaebae20eafa9c94851c1e0ce655a1d8636
Maximiliano Garcia <maxi.damian.garcia@gmail.com>
2014-06-01 15:07:46 -0300

    new file:   BGR/BGRSerranoREST/.gitignore 	new file:   BGR/BGRSerranoREST/src/main/java/ar/com/bgr/serrano/model/Sucursal.java 	modified:   BGR/BGRSerranoREST/src/main/webapp/html/index.html

---
+++ BGR/BGRSerranoREST/src/main/java/ar/com/bgr/serrano/model/Sucursal.java
@@ -0,0 +1,157 @@
+package ar.com.bgr.serrano.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "Sucursal")
+@Table(name = "Sucursal")
+public class Sucursal {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@Column(name = "name", nullable=false)
+	private String name;
+
+	@Column(name = "descripcion")
+	private String descripcion;
+
+	@Column(name = "observacion")
+	private String observacion;
+	
+	@Column(name = "calle", nullable=false)
+	private String calle;
+
+	@Column(name = "numero", nullable=false)
+	private String numero;
+	
+	@Column(name = "piso")
+	private String pisoradio;
+
+	@Column(name = "pais",nullable=false)
+	private String pais;
+
+	@Column(name = "provincia", nullable=false)
+	private String provincia;
+
+	@Column(name = "localidad", nullable=false)
+	private String localidad;
+
+	@Column(name = "zona", nullable=false)
+	private String zona;
+	
+	@Column(name = "codigoPostal", nullable=false)
+	private String codigoPostal;
+
+	@Column(name = "observacionDireccion", nullable=false)
+	private String observacionDireccion;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getPisoradio() {
+		return pisoradio;
+	}
+
+	public void setPisoradio(String pisoradio) {
+		this.pisoradio = pisoradio;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getZona() {
+		return zona;
+	}
+
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+	public String getObservacionDireccion() {
+		return observacionDireccion;
+	}
+
+	public void setObservacionDireccion(String observacionDireccion) {
+		this.observacionDireccion = observacionDireccion;
+	}
+
+}
