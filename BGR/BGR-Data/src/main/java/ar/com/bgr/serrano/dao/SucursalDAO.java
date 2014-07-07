3699fc2799dcb1597de0c3b5c361f5a02d77a250
Maximiliano Garcia <maxi.damian.garcia@gmail.com>
2014-07-07 20:18:11 -0300

    moved

--- BGR/BGR-Data/src/main/java/ar/com/bgr/serrano/dao/SucursalDAO.java
+++ BGR/BGR-Data/src/main/java/ar/com/bgr/serrano/dao/SucursalDAO.java
@@ -1,4 +1,4 @@
-package ar.com.bgr.serrano.dao.exception;
+package ar.com.bgr.serrano.dao;
 
 import java.util.List;
 
@@ -8,7 +8,6 @@ import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import ar.com.bgr.serrano.dao.AbstractDAO;
 import ar.com.bgr.serrano.model.Contacto;
 import ar.com.bgr.serrano.model.Sucursal;
 
